0b1f2ce7239bc6e584070ad26df197caa61ef316
Michael Hart <michael.hart1994@gmail.com>
2015-02-10 12:35:27 +0000

    Commit as Eclipse project

---
+++ src/interpreter.cpp
@@ -0,0 +1,14 @@
+/**
+ *
+ * File: interpreter.cpp
+ * Author: Michael Hart
+ * Date Created: 10/02/2015
+ * Description: Given command line arguments, will interpret given commands
+ * in a text file to perform commands on a custom linked list.
+ *
+ */
+
+
+int main(int argc, char **argv) {
+	return 0;
+}
